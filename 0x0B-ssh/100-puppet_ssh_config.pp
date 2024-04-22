95b76d43cbe6eda50b59ab88d6b4d9099561f9ab
MuhammadAfify <muhammadafefy@gmail.com>
2024-04-21 17:10:36 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,3 +4,8 @@ file { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  }
