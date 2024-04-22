1d3e2ef3aca60a36112ea3ccb54af60b46e39f8a
MuhammadAfify <muhammadafefy@gmail.com>
2024-04-21 17:08:13 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,4 @@
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
+# set up client SSH config file to connect to server without typing password
 file { 'Turn off passwd auth':
   ensure => 'present',
   path   => '~/.ssh/school',
