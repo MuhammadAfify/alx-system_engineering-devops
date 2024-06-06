ea632e9e60943843813fd396f75d0de7feeb5fd8
MuhammadAfify <muhammadafefy@gmail.com>
2024-06-05 18:49:40 -0700

    web

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Using strace find out why Apache is returning a 500 error fix it and then automate it
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
